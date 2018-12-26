20c9dcc34b6be47d53dbc2ac9f1e7e6a1101672c
Thibor <thibor@vp.pl>
2018-12-26 14:07:31 +0000

    Add files via upload

--- jsRapMoon.js
+++ jsRapMoon.js
@@ -7,22 +7,19 @@ $('.rapMoon').each(function(){
 (function($){
 $.fn.jsRapMoon = function(options){
 
-return this.each(function(){
-
-this.Render = function(){
-	this.opt = $.extend({},options);	
+return this.each(function(){	
 	var w = $(this).width();
-	$(this).height(w).addClass('rapMoon');
-	$('<div>').addClass('light').appendTo(this);
-	$('<div>').addClass('texture').appendTo(this);
 	var dark = '#444';
 	var synodicMonth = 29.530588853;
 	var curDate = new Date();
-	var orgDate = new Date(2012, 7, 31);
+	var orgDate = new Date(2012,7,31,0,0,0,0);
 	var milSec = curDate.getTime() - orgDate.getTime();
 	var daysElapsed = parseInt(milSec / 1000 / 60 / 60 / 24);
 	var daysSinceLast = daysElapsed % synodicMonth; 
-	var phase = daysSinceLast / synodicMonth;
+	var phase = daysSinceLast / synodicMonth;	
+	$(this).height(w).addClass('rapMoon');
+	$('<div>').addClass('light').appendTo(this);
+	$('<div>').addClass('texture').appendTo(this);
 	$('.light').css('filter','blur(' + (w / 24) + 'px)');
 	if(phase < 0.25)
 		$('.light').css({'border-right-width':(phase * 2 * w),'border-left-color':'#fff','background-color':'#fff','border-right-color':dark});
@@ -32,9 +29,6 @@ this.Render = function(){
 		$('.light').css({'border-right-width':((phase - 0.5) * 2 * w),'border-left-color':dark,'background-color':dark,'border-right-color':'#fff'});
 	else
 		$('.light').css({'border-left-width':((1 - phase) * 2 * w),'border-left-color':dark,'background-color':'#fff','border-right-color':'#fff'});
-}
-
-this.Render();
 })
 
 }})(jQuery);
